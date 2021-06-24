1ed24a908fbb5506a991d84afee9bb616bb50fe0
Richard Derer <richard.derer@gmail.com>
2021-06-24 21:53:42 +0200

    Should have all interfaces ...

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -3,25 +3,24 @@ pragma solidity ^0.6.12;
 
 import "hardhat/console.sol";
 
+// TEMP SOLUTION UNTIL following file gets integrated into npm package @aave/protocol-v2
+// https://github.com/aave/protocol-v2/blob/master/contracts/interfaces/IAaveIncentivesController.sol 
+import "./IAaveIncentivesController.sol"; 
+
 import {ILendingPool} from "@aave/protocol-v2/contracts/interfaces/ILendingPool.sol";
 import {IWETHGateway} from "@aave/protocol-v2/contracts/misc/interfaces/IWETHGateway.sol";
 import {IWETH} from "@aave/protocol-v2/contracts/misc/interfaces/IWETH.sol";
 import {IAToken} from "@aave/protocol-v2/contracts/interfaces/IAToken.sol";
 
-import {IAaveIncentivesController} from "@aave/protocol-v2/contracts/interfaces/IAaveIncentivesController.sol";
-
-// IDELNE TOTO CHCEM MAT !
-// https://github.com/aave/protocol-v2/blob/master/contracts/interfaces/IAaveIncentivesController.sol
-// preco nema ktualnu verziu ?
 
 contract Faucet {
     address private _owner;
     address private _botwallet;
 
     ILendingPool private immutable _aaveLendingPool;
-    IWETHGateway private _aaveWETHGateway;
-    IAaveIncentivesController private _aaveIncentivesController;
-    IAToken private _aaveamWMATIC; // Interest bearing token
+    IWETHGateway private immutable _aaveWETHGateway;
+    IAaveIncentivesController private immutable _aaveIncentivesController;
+    IAToken private immutable _aaveamWMATIC; // Interest bearing token
 
     IWETH private immutable _WMATIC; // wrapped matic
 
